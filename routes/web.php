0434c392c0e039310eb2b787d1720769664d8dfa
ChiaraMameli <mameli.chiara@libero.it>
2022-09-21 15:26:01 +0200

    created controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/comics', 'ComicController@index')->name('comics.index');
+Route::get('/comics/{id}', 'ComicController@show')->name('comics.show');
